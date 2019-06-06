d39c426cf5ee562fc184bf7f42f8e2037da6fd36
Kev Comedia <kevcomedia@gmail.com>
2019-06-06 16:02:30 +0800

    Updates

---
+++ precache-manifest.7d148b964c00a828e01d72e4501652f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8fbf8efaf44f3131a0a4d4d24f25e8",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "9434eed0cf49fa0e46cc",
+    "url": "/random-quote-machine/static/css/main.c7a28d58.chunk.css"
+  },
+  {
+    "revision": "6f26cdaf5fa093da5a63",
+    "url": "/random-quote-machine/static/js/2.ba1e478c.chunk.js"
+  },
+  {
+    "revision": "9434eed0cf49fa0e46cc",
+    "url": "/random-quote-machine/static/js/main.af5c801d.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
